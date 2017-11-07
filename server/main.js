ac528d987ba3393cb71bd40569c3c9bb7a6debd2
Bill Donovan <williamcdonovan@gmail.com>
2017-11-07 08:16:38 -0700

    added ejs and public folder

---
+++ server/main.js
@@ -0,0 +1,11 @@
+// ************************************
+// Dependencies
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res) {
+	res.render('./../app/index.ejs', {});
+})
+	.listen(7777);
